348995ed1551ebf30b7915af323a6f8b414627bd
Darriel Markerizal <darrielmarkerizal@gmail.com>
2025-04-03 08:24:00 +0700

    feat(dashboard) : cdn upload imgae integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["wheat-fish-905221.hostingersite.com"],
+  },
   // Gunakan salah satu opsi saja, tidak keduanya untuk paket yang sama
   experimental: {
     // Opsi ini membiarkan paket dijalankan sebagai external di server
