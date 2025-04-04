41570a1d46654c737833906e15ad196952d2d085
rasyid <rasyid@rasyids-MacBook-Air.local>
2025-04-04 23:41:30 +0700

    feat: layanan kami

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/home/hero";
+import Layanan from "@/components/home/layanan";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Layanan />
     </main>
   );
 }
