cf48f7d0c5519da8aa18dab78b107278de06f935
Darriel Markerizal <darrielmarkerizal@gmail.com>
2025-04-12 00:01:54 +0700

    secret key

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,7 +6,9 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-const secret = process.env.ENCRYPTION_SECRET || "default-secret-key";
+const secret =
+  process.env.ENCRYPTION_SECRET ||
+  "v9TAgQ5nmGj761ev9EuNJ6bKdrFTMQ4AdkvV3D91vVmJuqxkwWdknA6ZnvB9sA0WUjsApoK7bntmpQbQF0";
 
 export function encryptData(data) {
   const ciphertext = CryptoJS.AES.encrypt(
